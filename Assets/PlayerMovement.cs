3026b6099db10cc755551887f436528cdfe6dbb6
Monserrat Piña Lira <monserratpina80@gmail.com>
2026-02-18 13:58:16 -0600

    YA QUEDO :D

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,16 +1,21 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
+    [SerializeField] private float       moveSpeed = 5f;
+    private                  Rigidbody2D rb;
+    private                  Animator    animator;
+    private                  Vector2     moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() {
         rb =  GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
     }
 
 
@@ -20,6 +25,20 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Move (InputAction.CallbackContext context) {
+
+        animator.SetBool("IsWalking",true);
+        if (context.canceled)
+        {
+            animator.SetBool( "IsWalking",false);
+            animator.SetFloat("LastInputX",moveInput.x);
+            animator.SetFloat("LastInputY",moveInput.y);
+        }
+
         moveInput=context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX",moveInput.x);
+        animator.SetFloat("InputY",moveInput.y);
+        Debug.Log(moveInput);
+
     }
 }
